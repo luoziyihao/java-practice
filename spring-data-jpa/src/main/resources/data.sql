b7106116a96fb35b1446b80c19016a3a5ecdd524
luoziyihao <wangyiraoxiang@163.com>
2016-08-07 22:09:54 +0800

    comment init sql

--- spring-data-jpa/src/main/resources/data.sql
+++ spring-data-jpa/src/main/resources/data.sql
@@ -1,3 +1,4 @@
-insert into User (id, name, age) values (1, 'Joe', 32);
-insert into User (id, name, age) values (2, 'Sam', 28);
-insert into User (id, name, age) values (3, 'John', 43);
+;
+-- insert into User (id, name, age) values (1, 'Joe', 32);
+-- insert into User (id, name, age) values (2, 'Sam', 28);
+-- insert into User (id, name, age) values (3, 'John', 43);
