06c758c9619c929e9d47ad7f43bdc06e2d8ec77f
luoziyihao <wangyiraoxiang@163.com>
2017-06-20 22:18:21 +0800

    add comment

--- java-design-patterns/src/main/java/com/luozi/designpatterns/singleton/B.java
+++ java-design-patterns/src/main/java/com/luozi/designpatterns/singleton/B.java
@@ -1,7 +1,8 @@
 package com.luozi.designpatterns.singleton;
 
 public class B {
-    private static B INSTANCE;
+    // 加上volatile关键字, 保证属性的读在线程见可见
+    private static volatile B INSTANCE;
 
     private B() {
     }
@@ -12,6 +13,7 @@ public class B {
             // 只在 INSTANCE 为null的时候使用锁, 而不是在方法上使用 锁, 提高效率
             synchronized (B.class) {
                 // 在拿到索后, 还需要再次校验一下是否为 null, 因为可能好几个线程都走到拿到锁这一步了
+                // 要避免第二个及后面拿到锁的 线程重新生成对象, 同时这里使用 volatile 关键字, 保证值在线程中可见
                 if (INSTANCE == null) {
                     INSTANCE = new B();
                 }
