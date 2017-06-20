4960fd0abe1aa037cbdd7cf0f7b04cb8e8faef0a
luoziyihao <wangyiraoxiang@163.com>
2017-06-20 13:47:24 +0800

    numberToVoice 1.1

--- api/src/main/java/com/luozi/api/NumberUtils.java
+++ api/src/main/java/com/luozi/api/NumberUtils.java
@@ -1,6 +1,7 @@
 package com.luozi.api;
 
 import com.google.common.collect.ImmutableMap;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
 import java.util.Objects;
@@ -10,36 +11,41 @@ import java.util.Objects;
  */
 public class NumberUtils {
 
-    private static final Map<Integer, String> BIGNUMBER_VOICE_MAP = ImmutableMap.<Integer, String>builder()
+    private static final char NUMBER_PLACEHOLDER = '#';
+    private static final String NUMBER_PLACEHOLDER_STRING = "#";
+
+    private static final Map<Integer, Character> BIGNUMBER_VOICE_MAP = ImmutableMap.<Integer, Character>builder()
             .putAll(ImmutableMap.of(
-                    100000000, "亿"
-                    , 10000, "万"
+                    100000000, '亿'
+                    , 10000, '万'
+                    , 1, NUMBER_PLACEHOLDER
             ))
             .build();
 
-    private static final Map<Integer, String> NUMBER_VOICE_MAP = ImmutableMap.<Integer, String>builder()
+
+    private static final Map<Integer, Character> NUMBER_VOICE_MAP = ImmutableMap.<Integer, Character>builder()
             .putAll(ImmutableMap.of(
-                    1000, "千"
-                    , 100, "百"
-                    , 10, "十"
-                    , 1, ""
+                    1000, '千'
+                    , 100, '百'
+                    , 10, '十'
+                    , 1, NUMBER_PLACEHOLDER
             ))
             .build();
 
-    private static final Map<Integer, String> NUMBER_MAP = ImmutableMap.<Integer, String>builder()
+    private static final Map<Integer, Character> NUMBER_MAP = ImmutableMap.<Integer, Character>builder()
             .putAll(ImmutableMap.of(
-                    0, "零"
-                    , 1, "一"
-                    , 2, "二"
-                    , 3, "三"
-                    , 4, "四"
+                    0, '零'
+                    , 1, '一'
+                    , 2, '二'
+                    , 3, '三'
+                    , 4, '四'
             ))
             .putAll(ImmutableMap.of(
-                    5, "五"
-                    , 6, "六"
-                    , 7, "七"
-                    , 8, "八"
-                    , 9, "九"
+                    5, '五'
+                    , 6, '六'
+                    , 7, '七'
+                    , 8, '八'
+                    , 9, '九'
             ))
             .build();
 
@@ -53,35 +59,34 @@ public class NumberUtils {
      */
     public static String numberToVoice(int num) {
         StringBuilder builder = new StringBuilder();
-
-        for (Map.Entry<Integer, String> entry : BIGNUMBER_VOICE_MAP.entrySet()) {
+        if (num == 0) {
+            return ZERO_NUMBER_DESCRIPTION_STRING;
+        }
+        for (Map.Entry<Integer, Character> entry : BIGNUMBER_VOICE_MAP.entrySet()) {
             int count = num / entry.getKey();
             num = num % entry.getKey();
             if (count > 0) {
                 builder.append(processForFourBit(count))    // 数字按照四位分组处理
                         .append(entry.getValue());
             }
-
         }
 
-        builder.append(processForFourBit(num));
         CheckAndRemoveSpareFirstZero(builder);
         CheckAndRemoveSpareLastZero(builder);
-        return builder.toString();
+        return builder.toString()
+                .replaceAll(NUMBER_PLACEHOLDER_STRING, StringUtils.EMPTY)
+                .replaceAll(ZERO_NUMBER_REPLAX_REGEX, ZERO_NUMBER_DESCRIPTION_STRING)
+                ;
     }
 
-    private static void CheckAndRemoveSpareFirstZero(StringBuilder builder) {
-        int length = builder.length();
-        while (length > 1 && getFirstChar(builder).equals(NUMBER_MAP.get(zero))) {
-            length = builder.length();
-            if (Objects.equals(getFirstChar(builder), NUMBER_MAP.get(zero)) && length > 1) {
-                builder.deleteCharAt(zero);
-            }
-        }
+    private static final int zero = 0;
+    private static final char ZERO_NUMBER_DESCRIPTION = '零';
+    private static final String ZERO_NUMBER_DESCRIPTION_STRING = "零";
+    private static final String ZERO_NUMBER_REPLAX_REGEX = "[零]+";
 
-    }
 
     private static final int ZERO_CHECK_NUMBER = 1000;
+
     private static StringBuilder processForFourBit(int num) {
         StringBuilder builder = new StringBuilder();
         if (num < ZERO_CHECK_NUMBER) {  // 是否需要在第一位添零
@@ -90,13 +95,13 @@ public class NumberUtils {
         if (num >= HIGH) {
             throw new IllegalStateException("this method did't support number greater then " + HIGH);
         }
-        for (Map.Entry<Integer, String> entry : NUMBER_VOICE_MAP.entrySet()) {  // 拿到每一位的数 和 值
+        for (Map.Entry<Integer, Character> entry : NUMBER_VOICE_MAP.entrySet()) {  // 拿到每一位的数 和 值
             int count = num / entry.getKey();
             if (count >= 1) {
                 builder.append(NUMBER_MAP.get(count))
                         .append(entry.getValue());
-            } else if (checkIsAppendZero(builder, entry)) {
-                builder.append(NUMBER_MAP.get(count));
+            } else {    // 指定位数没有值, 补零, 如果存在多为为零的情况, 最后用正则统一替换掉
+                builder.append(ZERO_NUMBER_DESCRIPTION);
             }
             num = num % entry.getKey();
 
@@ -105,40 +110,40 @@ public class NumberUtils {
         return builder;
     }
 
-    private static final int zero = 0;
+    private static void CheckAndRemoveSpareFirstZero(StringBuilder builder) {
+        int length = builder.length();
+        while (length > 1 && getFirstChar(builder) == ZERO_NUMBER_DESCRIPTION) {
+            builder.deleteCharAt(zero);
+            length = builder.length();
+        }
+    }
 
     private static void CheckAndRemoveSpareLastZero(StringBuilder builder) {
         int length = builder.length();
-        while (length > 1 && getLastChar(builder).equals(NUMBER_MAP.get(zero))) {
+        while (length > 1 && getLastChar(builder) == ZERO_NUMBER_DESCRIPTION) {
+            builder.deleteCharAt(length - 1);
             length = builder.length();
-            if (Objects.equals(getLastChar(builder), NUMBER_MAP.get(zero)) && length > 1) {
-                builder.deleteCharAt(length - 1);
-            }
         }
     }
 
-    private static String getLastChar(StringBuilder builder) {
-        int length = builder.length();
-        if (length >= 1) {
-            return builder.charAt(length - 1) + "";
-        } else {
-            return "";
-        }
+    /**
+     * 方法在调用前必须保证 builder的length 大于 0
+     *
+     * @param builder
+     * @return
+     */
+    private static Character getLastChar(StringBuilder builder) {
+        return builder.charAt(builder.length() - 1);
     }
 
-    private static String getFirstChar(StringBuilder builder) {
-        int length = builder.length();
-        if (length >= 1) {
-            return builder.charAt(0) + "";
-        } else {
-            return "";
-        }
-    }
-
-    private static boolean checkIsAppendZero(StringBuilder builder, Map.Entry<Integer, String> entry) {
-        int length = builder.length();
-        return (length >= 1 && !Objects.equals(getLastChar(builder), NUMBER_MAP.get(zero)))
-                || entry.getKey() == 1;
+    /**
+     * 方法在调用前必须保证 builder的length 大于 0
+     *
+     * @param builder
+     * @return
+     */
+    private static char getFirstChar(StringBuilder builder) {
+        return builder.charAt(0);
     }
 
 
